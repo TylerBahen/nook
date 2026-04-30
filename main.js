9382a080cbbb3c8d2094fc856ab629e16f56711f
Tyler Bahen <tylerbahen@gmail.com>
2026-04-30 13:17:55 -0600

    Installed npm packages

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //This Is The Server (Backend)
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+const CryptoJS = require("crypto-js");
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
