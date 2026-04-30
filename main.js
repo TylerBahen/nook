f3e8cd964515292ce13201c96f66e7e7750e8408
Tyler Bahen <tylerbahen@gmail.com>
2026-04-30 13:41:46 -0600

    Infrastructure Update

--- main.js
+++ main.js
@@ -8,3 +8,28 @@ const CryptoJS = require("crypto-js");
 const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
+
+app.get('/',function (req,res){
+    res.sendFile(__dirname + '/pages/landing.html')
+})
+app.get('/login',function (req,res){
+    res.sendFile(__dirname + '/pages/login.html')
+})
+app.get('/signup',function (req,res){
+    res.sendFile(__dirname + '/pages/signup.html')
+})
+app.get('/nook',function (req,res){
+    res.sendFile(__dirname + '/pages/nook.html')
+})
+app.use('/scripts', express.static(__dirname + '/scripts'))
+app.use('/styles', express.static(__dirname + '/styles'))
+app.use('/images', express.static(__dirname + '/images'))
+
+io.on('connection',function(client){
+    client.emit('ioconnect')
+})
+
+server.listen(8889)
+console.log('Server listening on 8889')
+console.log('http://127.0.0.1:8889/nook')
+console.log(new Date(Date.now()).toString())
